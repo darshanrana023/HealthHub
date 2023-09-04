03d73029cf292463e75d5de228c27d66ff174825
darshanrana023 <126544748+darshanrana023@users.noreply.github.com>
2023-09-04 15:07:56 +0530

    methods are called

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import csv
 from flask import Flask,render_template,request,redirect,url_for
 app=Flask(__name__)
@@ -7,25 +8,59 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/register', methods=['POST'])
-def register():
-    name = request.form['name']
-    phone = request.form['phone']
-    email = request.form['email']
-    password = request.form['password']
-
-        # Write the user data to the CSV file
-    with open('users.csv', mode='a', newline='') as csv_file:
+# Check if the CSV file exists
+if not os.path.isfile('users.csv'):
+    with open('users.csv', mode='w', newline='') as csv_file:
         fieldnames = ['Name', 'Phone', 'Email', 'Password']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        with open('users.csv', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                if row['Email'] == email and row['Password'] == password:
+                    return "Login successful!"
+    
+    return "Invalid email or password. <a href='/'>Go back to Home</a>"
+
+# ...
+
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        print("POST request received")
+    else:
+        print("GET request received")
+    # Rest of your registration code here
+
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Write the user data to the CSV file
+        with open('users.csv', mode='a', newline='') as csv_file:
+            fieldnames = ['Name', 'Phone', 'Email', 'Password']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
             # Write the header if the file is empty
-    if csv_file.tell() == 0:
-        writer.writeheader()
-    
-    writer.writerow({'Name': name, 'Phone': phone, 'Email': email, 'Password': password})
-    return redirect(url_for('registration_success'))
+            if csv_file.tell() == 0:
+                writer.writeheader()
+
+            writer.writerow({'Name': name, 'Phone': phone, 'Email': email, 'Password': password})
+
+        return redirect(url_for('registration_success'))
+
     return render_template('register.html')
+
 # Route to display a registration success page
 
 @app.route('/registration-success')
